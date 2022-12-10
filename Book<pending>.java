dbfaed42cced5d9fc6efdaf76d018ac2a39933e9
Sai Krishna <99529667+saikrishna7783@users.noreply.github.com>
2022-12-10 13:18:17 +0530

    Update Book<pending>.java

--- Book<pending>.java
+++ Book<pending>.java
@@ -1,12 +1,5 @@
 import java.util.Scanner;
 
-class Book{
-    String name,author;
-    int price,num_pages;
-    Book(String n,String a,int p,int no){
-        name=n;
-        author=a;import java.util.Scanner;
-
 class Book {
     String name, author;
     int price, num_pages;
@@ -21,7 +14,6 @@ class Book {
     // price = sc.nextInt();
     // System.out.print("Enter the number of pages of the book : ");
     // num_pages = sc.nextInt();
-    // sc.close();
     // }
 
     void assign() {
@@ -34,13 +26,12 @@ class Book {
         price = sc.nextInt();
         System.out.print("Enter the number of pages of the book : ");
         num_pages = sc.nextInt();
-        sc.close();
     }
 
     public String toString() {
         return ("------------------------------------------------------------\nBook Title : " + name + "\nAuthor : "
                 + author + "\nPrice : " + price + "\nNo of pages : " + num_pages
-                + "\n------------------------------------------------------------");
+                + "\n------------------------------------------------------------\n");
     }
 }
 
@@ -49,37 +40,16 @@ class hi {
         System.out.print("Enter the number of books : ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.close();
-        Book b[] = new Book[n];
+        Book[] b = new Book[n];
         int i;
         for (i = 0; i < n; i++) {
-            System.out.print("Enter the details of Book " + i + 1 + "\n");
+            System.out.print("\nEnter the details of Book " + (i + 1) + "\n");
+            b[i] = new Book();
             b[i].assign();
         }
         for (i = 0; i < n; i++) {
-            System.out.print("The details of Book " + i + 1 + " are :\n");
+            System.out.print("\nThe details of Book " + (i + 1) + " are :\n");
             System.out.print(b[i]);
-        }
-    }
-}
-        price=p;
-        num_pages=no;
-    }
-    void toString(){
-        System.out.println("The name of the book is : "+name);
-        System.out.println("The author of the book is : "+author);
-        System.out.println("The price of the book is : "+price);
-        System.out.println("The number of pages in the book is : "+num_pages);
-    }
-}
-
-class ex{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of books : ");
-        int n = sc.nextint();
-        for(int i=0;i<n;i++){
-            Book Booki = new Book();
         }
     }
 }

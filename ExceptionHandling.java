0fe76c4c36b9705b96d2284a015186f60f8875e0
Sai Krishna <99529667+saikrishna7783@users.noreply.github.com>
2023-01-06 12:22:04 +0530

    Create ExceptionHandling.java

---
+++ ExceptionHandling.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.print("We will perform division now :");
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("\nEnter the Dividend : ");
+            int num1 = Integer.parseInt(sc.next());
+            System.out.print("Enter the Divisor : ");
+            int num2 = Integer.parseInt(sc.next());
+            double result = num1 / num2;
+            System.out.println("The quotient is : " + result);
+        } catch (NumberFormatException e) {
+            System.out.print("Enter a valid integer value!\n" + e + "\n");
+        } catch (ArithmeticException e) {
+            System.out.print("Cannot perform the division operation!\n" + e + "\n");
+        }
+        sc.close();
+    }
+}
